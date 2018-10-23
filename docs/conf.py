20bff7dd29bd1c911b49439b7f441a87db0562e3
SkySom <skylar.som@gmail.com>
2018-10-23 12:43:31 -0400

    Change Theme

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -136,7 +136,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'project', u'Project Documentation',
+    (master_doc, 'project', u'BGC/CAD/MA/TAC Documentation',
      [author], 1)
 ]
 
